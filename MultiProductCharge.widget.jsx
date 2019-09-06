246e7d085ef85f1cf47e318f976b36666970eaf5
Ed <edd_undead@tut.by>
2019-09-06 20:30:44 +0300

    Update MultiProductCharge.widget.jsx

--- MultiProductCharge.widget.jsx
+++ MultiProductCharge.widget.jsx
@@ -1,69 +1,34 @@
 <BPUI.Page>
 	<div className="main-div-page">
-    <div className="div-flex   my-15">
-        <div className="div-flex-inner basis50">
-{/*
-<BPUI.InputField key="01112" label="Account Name  " disabled variable={new BPUI.ReferenceObject('- Not found -')} /> 
-<br /><br />
-<BPUI.InputField key="01113"  label="Invoice period " disabled variable={new BPUI.ReferenceObject('- Not selected -')} /> */}
-            <div className="text-big">
-                Account Name: <span className="text-black_" id="account-info-name"> {accountInfo.Name} </span>
-                <br /><br />
-                Invoice period: <span  className="text-black_"  id="account-info-period"> {invoiceDate.start} -
-                    {invoiceDate.end}</span>
-				<br /><br />
-                Invoice ID: <span  className="text-black_" id="account-info-invoiceid"> - Not selected - </span>
-            </div>
+    <div className="div-flex my-15">
+        <div className="div-flex-inner">
+        <BPUI.Panel style ={{width: 900 + "px"}}>
+        <BPUI.PanelRow>
+<BPUI.OutputField key="01112" label="Account Name" className='pr-3'  variable={invoiceInfoObj} field={'accountName'}/> 
+ 
+<BPUI.OutputField key="01113"  label="Invoice period" className='pr-3'    variable={invoiceInfoObj} field={'invoicePeriod'} />  
+ 
+<BPUI.OutputField key="01114"  label="Invoice ID"  className='pr-3'   variable={invoiceInfoObj} field={'invoiceId'}  />  
+</BPUI.PanelRow>
+ </BPUI.Panel>
         </div>
     </div>
- <BPUI.FormLayout submitAction={WIDGET_MODE === 'insert'?doSave:doUpdate} cancelAction={cancel}>
-    {WIDGET_MODE === 'insert'?
+ <BPUI.FormLayout submitAction={doSaveAndUpdate} cancelAction={cancel}>
     <div className="div-flex main-div">
         <div className="div-flex-inner basis100">
-            <BPUI.EmbeddedList  variable={activities} name="activities" width="100%"
+            <BPUI.EmbeddedList variable={lastactivities} name="activities" width="100%"
                 onCellBlur={calculateRate_} onAdd={addActivity}>
-                <BPUI.TableColumn name="ProductId" index="2" label="Product Name" />
-                <BPUI.TableColumn name="SubscriptionFromDate" type="DATE_SELECTOR" index="1"
+                <BPUI.TableColumn  name="ProductId" index="2" label="Product" />
+                <BPUI.TableColumn  name="ActivityDate" type="DATE_SELECTOR" index="8" displayTransform={formatDateUI} label="Activity Date" />
+                <BPUI.TableColumn className={"disabled hide"} name="SubscriptionFromDate" type="DATE_SELECTOR" index="1"
                     displayTransform={formatDateUI} label="From Date" />
-                <BPUI.TableColumn name="SubscriptionToDate" type="DATE_SELECTOR" index="1"
+                <BPUI.TableColumn className={"disabled hide"} name="SubscriptionToDate" type="DATE_SELECTOR" index="1"
                     displayTransform={formatDateUI} label="To Date" />
                 <BPUI.TableColumn name="Quantity" index="3" label="Quantity" />
                 <BPUI.TableColumn name="Rate" index="4" label="Rate" />
-                <BPUI.TableColumn className={"disabled"} name="RatedAmount" index="5" label="Cost" />
+                <BPUI.TableColumn className={"disabled"} name="Cost" index="5" label="Cost" />
                 <BPUI.TableColumn name="TaxCost" index="6" label="Tax" />
                 <BPUI.TableColumn className={"disabled"} name="TotalCost" index="7" label="Total Cost" />
-                <BPUI.TableColumn name="ActivityDate" type="DATE_SELECTOR" index="8" displayTransform={formatDateUI}
-                    label="Activity Date" />
-            </BPUI.EmbeddedList>
-        </div>
-        <div className="div-flex-inner basis100">
-            <div className="footer-buttons">
-                <span id="msg-info" className="footer-buttons-label text-blue hide">Saving data in a progress... </span>
-                <span id="msg-succ" className="footer-buttons-label text-success success-msg hide "><i
-                        className="fa fa-check-circle"></i> Data was saved successfully!</span>
-                <span id="msg-fail" className="footer-buttons-label text-danger failed-msg hide "><i
-                        className="fa fa-warning"></i> Error occurred while data saving!</span>
-            </div>
-        </div>
-    </div>
-    :null}
-    {WIDGET_MODE === 'update'?
-    <div className="div-flex main-div">
-        <div className="div-flex-inner basis100">
-            <BPUI.EmbeddedList canAdd={false} variable={lastactivities} name="activities" width="100%"
-                onCellBlur={calculateRate_}>
-                <BPUI.TableColumn className={"disabled"} name="ProductId" index="2" label="Product Name" />
-                <BPUI.TableColumn className={"disabled"} name="SubscriptionFromDate" type="DATE_SELECTOR" index="1"
-                    displayTransform={formatDateUI} label="From Date" />
-                <BPUI.TableColumn className={"disabled"} name="SubscriptionToDate" type="DATE_SELECTOR" index="1"
-                    displayTransform={formatDateUI} label="To Date" />
-                <BPUI.TableColumn name="Quantity" index="3" label="Quantity" />
-                <BPUI.TableColumn name="Rate" index="4" label="Rate" />
-                <BPUI.TableColumn className={"disabled"} name="RatedAmount" index="5" label="Cost" />
-                <BPUI.TableColumn name="TaxCost" index="6" label="Tax" />
-                <BPUI.TableColumn className={"disabled"} name="TotalCost" index="7" label="Total Cost" />
-                <BPUI.TableColumn className={"disabled"} name="ActivityDate" type="DATE_SELECTOR" index="8" displayTransform={formatDateUI}
-                    label="Activity Date" />
             </BPUI.EmbeddedList>
         </div>
         <div className="div-flex-inner basis100">
@@ -73,11 +38,10 @@
                 <span id="msg-succ_" className="footer-buttons-label text-success success-msg hide "><i
                         className="fa fa-check-circle"></i> Data was saved successfully!</span>
                 <span id="msg-fail_" className="footer-buttons-label text-danger failed-msg hide "><i
-                        className="fa fa-warning"></i> Error occurred while data saving!</span>
+                        className="fa fa-warning"></i> Error occurred while data saving! Check fields values in the table and try again.</span>
             </div>
         </div>
     </div>
-    :null}
 <div className="div-flex process-error hide">
 <div className="div-flex-inner basis100 text-danger failed-msg">
 <i className="fa fa-warning" aria-hidden="true"/> You can't Insert/Update Products on Invoice with status CLOSED or APPROVED.
@@ -156,17 +120,35 @@ margin:15px 0;
 .text-black{
 color:#000
 }
+.pr-3{
+    padding-left: 15px;
+}
+.div-flex-inner nobr span:last-child{
+      width: 100px !important;
+    text-align: right;
+    display: inline-block;
+    white-space: nowrap;
+}
+
+tr.edited td{
+    background:#dcf4fc;
+}
 ___________________________________________________________________________________________________
 BPSystem.initialize();
 window.billingProfile = new BPUI.ReferenceObject();
 window.invoice = new BPUI.ReferenceObject();
 window.account = new BPUI.ReferenceObject();
-window.activities = new BPUI.ReferenceObject();//ReferenceObject test
+window.invoiceInfoObj = new BPUI.ReferenceObject();
+invoiceInfoObj.set(new Invoice());
+invoiceInfoObj.get().accountName = ' - Not found -';
+invoiceInfoObj.get().invoicePeriod = ' - Not selected -';
+invoiceInfoObj.get().invoiceId = ' - Not selected -';
+
 window.lastactivities = new BPUI.ReferenceObject();
 window.accountInfo = {Name:'- Not found -'};
 window.invoiceDate = {start:'- Not selected '};
 window.INVOICE_STATUS = null;
-const WIDGET_MODE = localStorage.MultiProductCharge_mode || localStorage.setItem('MultiProductCharge_mode','insert');
+window.noCharges = true;
 const accountId = BPSystem.nodeKey; //1
 const activityId = BPSystem.nodeKey; //1
 const formatDateUI = (val) => val?moment(val).format('MM/DD/YYYY'):val;
@@ -184,9 +166,11 @@ invoiceDate = {
     start: formatDateUI(formatDateDB(invoice_.BillingCycleStartDate)), 
     end: formatDateUI(formatDateDB(invoice_.BillingCycleEndDate))
 };
-document.querySelector('#account-info-name').innerHTML = account_.Name;  
-document.querySelector('#account-info-period').innerHTML = `${invoiceDate.start} - ${invoiceDate.end}`;
-document.querySelector('#account-info-invoiceid').innerHTML = invoiceId_;
+
+invoiceInfoObj.get().accountName = account_.Name;
+invoiceInfoObj.get().invoicePeriod = `${invoiceDate.start} - ${invoiceDate.end}`;
+invoiceInfoObj.get().invoiceId = invoiceId_;
+ 
     
 if (!(INVOICE_STATUS=='OPEN' || INVOICE_STATUS=='CURRENT')){
        document.querySelector('.process-error').classList.remove('hide');
@@ -194,17 +178,29 @@ if (!(INVOICE_STATUS=='OPEN' || INVOICE_STATUS=='CURRENT')){
        const submitBtns = document.querySelectorAll('.main-div-page a[name="submitForm"]');
        for (let btn of submitBtns) btn.classList.add('hide'); 
 }
-WIDGET_MODE==='update' ? 
-window.lastactivities.set(BPConnection.ACTIVITY.query('SELECT Id,ProductId,SubscriptionFromDate,SubscriptionToDate,Quantity,Rate,RatedAmount,TaxCost,TotalCost,ActivityDate from Activity WHERE  AccountId='+account_.Id+' AND InvoiceId='+invoice_.Id+' ORDER BY Updated DESC').collection()):null;
 
-    //init new activity by default
-    activities.set(new BPConnection.BPCollection([{}], new Activity()));
-    //default the activity dates to today
-    activities.get().forEach(function (element, index, allArray) {
-        element.ActivityDate = formatDateDB(invoiceDate.start);
-        element.SubscriptionFromDate = formatDateDB(invoiceDate.start);
-        element.SubscriptionToDate = formatDateDB(invoiceDate.end);
-    });
+try{
+const lastactivities__ = await BPConnection.ACTIVITY.query(`
+SELECT Id,ProductId,SubscriptionFromDate,SubscriptionToDate,
+Quantity,to_char(Rate, 'FM999999990.00') as Rate, to_char(Cost, 'FM999999990.00') as Cost,
+to_char(TaxCost, 'FM999999990.00') as TaxCost,to_char(TotalCost, 'FM999999990.00') as TotalCost,ActivityDate 
+FROM Activity 
+WHERE  AccountId=${account_.Id} 
+AND InvoiceId=${invoice_.Id} 
+ORDER BY Updated DESC`).collection();
+window.lastactivities.set(lastactivities__);
+window.noCharges = false;
+}catch(e){
+    window.noCharges = true;
+        //init new activity by default
+        window.lastactivities.set(new BPConnection.BPCollection([{}], new Activity()));
+        //default the activity dates to today
+        window.lastactivities.get().forEach(function (element, index, allArray) {
+            element.ActivityDate = formatDateDB(invoiceDate.start);
+            element.SubscriptionFromDate = formatDateDB(invoiceDate.start);
+            element.SubscriptionToDate = formatDateDB(invoiceDate.end);
+        });
+}
     //init account and billing profile of account
     account.set(BPSystem.toBPObject({}, new Account()));
     account.get().Id = accountInfo.Id;
@@ -213,62 +209,80 @@ window.lastactivities.set(BPConnection.ACTIVITY.query('SELECT Id,ProductId,Subsc
     invoice.set(invoice_);
 }
 
-init();
+
 
 function cancel() {
 	window.add_attr_submit('SET_FORM_VIEW', 'form_type_in', 'FL')
 }
 
-const doSave = async()=> {
-document.querySelector('#msg-info').classList.remove('hide');
-document.querySelector('#msg-fail').classList.add('hide');
-document.querySelector('#msg-succ').classList.add('hide');
-const newActivities = activities.get().elements.map(el=>({
-                InvoiceId: invoice.get().Id,
-                AccountId: accountInfo.Id,
-                ProductId: el.ProductId,
-                ActivityDate: el.ActivityDate,
-                SubscriptionFromDate:el.SubscriptionFromDate,
-                SubscriptionToDate:el.SubscriptionToDate,
-                Quantity: el.Quantity,
-                RateOverride: el.Rate,
-                CostOverride: el.RatedAmount,
-    			TaxCost:el.TaxCost,
-    			TotalCost:el.TotalCost
-            }));
-const resultActivity = await BPSystem.toBPCollection(newActivities, BPConnection.Activity).create(true);
-    if (resultActivity[0].ErrorCode == "0"){
-        window.onbeforeunload = true;
-        document.querySelector('#msg-succ').classList.remove('hide');
-        window.location = "admin.jsp?name=BILLING_INVOICE_DETAIL_NEW&key=" + resultActivity[0].Id + "&mode=L";
-    }else{
-        document.querySelector('#msg-fail').classList.remove('hide');
-        console.error("Fail", resultActivity); 
-    } 
-	document.querySelector('#msg-info').classList.add('hide');
-}
-    
-const doUpdate = async () => {
+   
+const doSaveAndUpdate = async () => {
 	document.querySelector('#msg-info_').classList.remove('hide');
 	document.querySelector('#msg-fail_').classList.add('hide');
-	document.querySelector('#msg-succ_').classList.add('hide');
-	const resultActivity = await lastactivities.get().update();
-		if (resultActivity[0].ErrorCode == "0"){
-    //const res_ = await BPConnection.REPROCESS_QUEUE.create({InvoiceId:resultActivity[0].Id})
-    
-			window.onbeforeunload = true;
-			document.querySelector('#msg-succ_').classList.remove('hide');
-		    window.location = "admin.jsp?name=BILLING_INVOICE_DETAIL_NEW&key=" + resultActivity[0].Id + "&mode=L";
-		}else{
-			document.querySelector('#msg-fail_').classList.remove('hide');
-			console.error("Fail", resultActivity); 
-		} 
+    document.querySelector('#msg-succ_').classList.add('hide');
+    const accountId = account.get().Id;
+    const invoiceId = invoice.get().Id;
+    let upsertElements = [];
+    let createdElements = window.noCharges? lastactivities.get().elements:lastactivities.get().createdelements;
+    if (createdElements.length>0){
+     //   alert('created = '+createdElements.length);
+        upsertElements.push(...createdElements);
+    }else{
+      //  alert('not created');
+    }
+    let updatedElements = lastactivities.get().updatedelements;
+    if (updatedElements.length>0){
+     //   alert('updated = '+updatedElements.length); 
+        upsertElements.push(...updatedElements);
+    }else{
+    //    alert('not updated');
+    }
+    let upsertElements_ = BPSystem.toBPCollection(JSON.parse(JSON.stringify(upsertElements))).elements;
+    upsertElements_.forEach(el=>{
+        el.AccountId = accountId;
+        el.InvoiceId = invoiceId;
+        delete el.Rate;
+        delete el.Cost;
+    });
+    console.log(upsertElements_,upsertElements)
+
+   // alert('count upsert = '+upsertElements.length);
+   // console.log('upserted',upsertElements);
+    if (upsertElements_.length>0) {
+    try {    
+    const resp = await BPConnection.Activity.upsert(upsertElements_);
+    document.querySelector('#msg-succ_').classList.remove('hide');
+    window.onbeforeunload = true;
+    window.location = "admin.jsp?name=BILLING_INVOICE_DETAIL_NEW&key=" + resp[0].Id + "&mode=L";
+    }catch(e){
+        console.log(e);
+        document.querySelector('#msg-fail_').classList.remove('hide');  
+    }
+    }else
+    {
+        alert('You have to change / insert Products and try again.')
+    }
 	document.querySelector('#msg-info_').classList.add('hide');
 }
-    
+
+ 
+
+const checkFieldsFilled = (objActivities,fieldsArr = ['Quantity','Rate']) => {
+    const objA = objActivities.get().elements;
+    for (obj of objA){
+        for(field of fieldsArr){
+            if (objA[field]==='' /**empty or not a number format */){ 
+                alert('Check field: '+field+' of Product'+objA.Id);
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 function calculateRate_(row, column, event, scope) {
-	let activityCollection = WIDGET_MODE==='insert'? activities.get():lastactivities.get();
-    const columnsCalc = [0,3/*,4,6*/];
+    let activityCollection =  lastactivities.get();
+    const columnsCalc = [4/*,4,6*/];
     let rowElement = activityCollection.elements[row];
     if (rowElement.Quantity /*&& rowElement.Rate */   && (columnsCalc.includes(column))) {
 
@@ -286,10 +300,10 @@ function calculateRate_(row, column, event, scope) {
                     //rowElement.RatedAmount = formatAmount($(rateDetails).find('RateDetailsRow > RatedAmount').text());
                     rowElement.TaxCost = formatAmount(res.TaxAmount);
                     if (rowElement.Rate && rowElement.Quantity) {
-                        rowElement.RatedAmount = formatAmount(rowElement.Quantity * rowElement.Rate);
+                        rowElement.Cost = formatAmount(rowElement.Quantity * rowElement.Rate);
                         rowElement.RateOverride = rowElement.Rate;
-                        rowElement.CostOverride = rowElement.RatedAmount;
-                        rowElement.TotalCost = formatAmount(+rowElement.RatedAmount + (Number.isNaN(+rowElement.TaxCost)?0:+rowElement.TaxCost));
+                        rowElement.CostOverride = rowElement.Cost;
+                        rowElement.TotalCost = formatAmount(+rowElement.Cost + (Number.isNaN(+rowElement.TaxCost)?0:+rowElement.TaxCost));
     					return;
                     }
                 })
@@ -303,10 +317,10 @@ function calculateRate_(row, column, event, scope) {
     }
     //if user change cells manually
     if (rowElement.Rate && rowElement.Quantity) {
-       rowElement.RatedAmount = formatAmount(rowElement.Quantity * rowElement.Rate);
+       rowElement.Cost = formatAmount(rowElement.Quantity * rowElement.Rate);
        rowElement.RateOverride = rowElement.Rate;
-       rowElement.CostOverride = rowElement.RatedAmount;
-       rowElement.TotalCost = formatAmount(+rowElement.RatedAmount + (Number.isNaN(+rowElement.TaxCost)?0:+rowElement.TaxCost));
+       rowElement.CostOverride = rowElement.Cost;
+       rowElement.TotalCost = formatAmount(+rowElement.Cost + (Number.isNaN(+rowElement.TaxCost)?0:+rowElement.TaxCost));
     }
 }
  
@@ -315,7 +329,7 @@ const checkAccount = () =>(account && account.get() && account.get().Id);
 function addActivity(index) {
     window.onbeforeunload = function(){
   return 'Are you sure you want to leave? All Data will be loss.'};
-    const activitiesData = activities.get();
+    const activitiesData = lastactivities.get();
     const accountId = account.get().Id;
     const invoiceId = invoice.get().Id;
     activitiesData.addNew({}, index + 1); // next item
@@ -333,3 +347,4 @@ BPUI.afterRender = () => {
 let menus = document.querySelectorAll('.formButtons');
 for (let menu of menus) menu.setAttribute('width','100%');
 }
+init();
