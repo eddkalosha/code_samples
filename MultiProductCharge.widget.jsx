b171742cdd01d05c290ce7d039c7488d34c12b8b
Ed <edd_undead@tut.by>
2019-11-12 15:16:06 +0300

    Update MultiProductCharge.widget.jsx

--- MultiProductCharge.widget.jsx
+++ MultiProductCharge.widget.jsx
@@ -19,10 +19,6 @@
 				onCellBlur={calculateRate_}  onDel={delActivity} onAdd={addActivity} onRowClick={checkProductType}>
 				<BPUI.TableColumn  name="ProductId" index="2" onUpdate={checkProductRatig} label="Product" type="LOOKUP" />
                 <BPUI.TableColumn  name="ActivityDate" type="DATE_SELECTOR" index="8"   label="Activity Date" />
-                <BPUI.TableColumn className={"disabled hide"} name="SubscriptionFromDate" type="DATE_SELECTOR" index="1"
-                    displayTransform={formatDateUI} label="From Date" />
-                <BPUI.TableColumn className={"disabled hide"} name="SubscriptionToDate" type="DATE_SELECTOR" index="1"
-                    displayTransform={formatDateUI} label="To Date" />
                 <BPUI.TableColumn name="Quantity" index="3" label="Quantity" />
                 <BPUI.TableColumn name="Rate" index="4" label="Rate" />
                 <BPUI.TableColumn className={"disabled"} name="Cost" index="5" label="Cost" />
@@ -172,16 +168,47 @@ invoiceInfoObj.get().invoicePeriod = ' - Not selected -';
 invoiceInfoObj.get().invoiceId = ' - Not selected -';
 
 window.lastactivities = new BPUI.ReferenceObject();
+window.lastactivities_copy = new BPUI.ReferenceObject();
 window.accountInfo = {Name:'- Not found -'};
 window.invoiceDate = {start:'- Not selected '};
 window.INVOICE_STATUS = null;
 window.noCharges = true;
-const accountId =  BPSystem.nodeKey; //1
-const activityId =   BPSystem.nodeKey; //1
+const accountId = BPSystem.nodeKey; //1
+const activityId = BPSystem.nodeKey; //1
 const formatDateUI = (val) => val?moment(val).format('MM/DD/YYYY'):val;
 const formatDateDB = (val) => val?moment(val).format('YYYY-MM-DD'):moment(new Date()).format('YYYY-MM-DD');
 const formatAmount = (amount) => amount?parseFloat(amount).toFixed(2):"0.00";
+const DATE_TIME_FORMAT = 'MM/DD/YYYY';
+const DISABLED_PRODUCTS = ['PER INVOICE', 'LATE FEE', 'DISCOUNT', 'CONDITIONAL PRODUCT CHARGE', 'TAX', 'TRIAL', 'REVENUE COMMITMENT','FORMULA'];
 
+function formatLocalDateTime(value) {
+    if (value) {
+        let dateObj = (typeof value === "string" ? new Date(Number(value)) : value);
+        let timeZone = BPSystem.userTimeZoneName;
+        let dateTime = (timeZone ? moment(dateObj).tz(timeZone) : moment(dateObj));
+        return dateTime.format(DATE_TIME_FORMAT);
+    }
+    return value;
+}
+
+function formatUTCDateTime(value) {
+    if (value) {
+        var dateObj = (typeof value === "string" ? new Date(Number(value)) : value);
+        var timeZone = BPSystem.userTimeZoneName;
+        var dateTime = (timeZone ? moment.utc(dateObj).tz(timeZone) : moment.utc(dateObj).utcOffset(-new Date().getTimezoneOffset()));
+        return dateTime.format(DATE_TIME_FORMAT);
+    }
+    return value;
+}
+
+function parseDateTimeToUTC(value) {
+    if (value) {
+        var timeZone = BPSystem.userTimeZoneName;
+        var dateTime = (timeZone ? moment.tz(value, DATE_TIME_FORMAT, timeZone).utc() : moment(value).utc());
+        return dateTime.format(DATE_TIME_FORMAT);
+    }
+    return value;
+}
 async function init() {
 const invoiceId_ =  await BPSystem.getSelectedEntityAsync("BILLING_INVOICE");
 const invoice_ = await BPConnection.Invoice.retrieveFilteredAsync('Id='+invoiceId_).single();
@@ -208,43 +235,36 @@ if (!(INVOICE_STATUS=='OPEN' || INVOICE_STATUS=='CURRENT')){
 
 try{
 const lastactivities__ = await BPConnection.ACTIVITY.query(`
-SELECT Id,ProductId,SubscriptionFromDate,SubscriptionToDate,
-Quantity,to_char(Rate, 'FM999999990.00') as Rate, to_char(Cost, 'FM999999990.00') as Cost,
-to_char(TaxCost, 'FM999999990.00') as TaxCost,to_char(TotalCost, 'FM999999990.00') as TotalCost,ActivityDate 
-FROM Activity 
-WHERE  AccountId=${account_.Id} 
-AND InvoiceId=${invoice_.Id} 
-ORDER BY Updated DESC`).collection();
+SELECT a.Id,a.ProductId,a.SubscriptionFromDate,a.SubscriptionToDate,
+a.Quantity,to_char(a.Rate, 'FM999999990.00') as Rate, to_char(a.Cost, 'FM999999990.00') as Cost,
+to_char(a.TaxCost, 'FM999999990.00') as TaxCost,to_char(a.TotalCost, 'FM999999990.00') as TotalCost,
+to_char(a.ActivityDate,'MM/DD/YYYY') as ActivityDate ,
+r.RatingMethodType
+FROM Activity a, Rating_Method r
+WHERE  a.AccountId=${account_.Id} 
+AND a.InvoiceId=${invoice_.Id} 
+AND r.Id = a.RatingMethodId
+ORDER BY a.Updated DESC`).collection();
 window.lastactivities.set(lastactivities__);
 window.noCharges = false;
 }catch(e){
     window.noCharges = true;
         //init new activity by default
-        window.lastactivities.set(new BPConnection.BPCollection([{}], new Activity()));
-        //default the activity dates to today
-        window.lastactivities.get().forEach(function (element, index, allArray) {
-            element.ActivityDate = formatDateDB(invoiceDate.start);
-            element.SubscriptionFromDate = formatDateDB(invoiceDate.start);
-            element.SubscriptionToDate = formatDateDB(invoiceDate.end);
-        });
+        window.lastactivities.set(new BPConnection.BPCollection([{}], new Activity())); 
 }
     //init account and billing profile of account
     account.set(BPSystem.toBPObject({}, new Account()));
     account.get().Id = accountInfo.Id;
     billingProfile.set(BPConnection.BillingProfile.retrieveFiltered("AccountId=" + accountInfo.Id).single());
+    window.lastactivities_copy = BPSystem.toBPCollection(JSON.parse(JSON.stringify(window.lastactivities.get().elements))).elements
     //init selected invoice
     invoice.set(invoice_);
     document.querySelector('.main-div-page').classList.remove('hide');
     document.querySelector('.info-loading').classList.add('hide');
 }
 
+const cancel = () => window.add_attr_submit('SET_FORM_VIEW', 'form_type_in', 'FL');
 
-
-function cancel() {
-	window.add_attr_submit('SET_FORM_VIEW', 'form_type_in', 'FL')
-}
-
-   
 const doSaveAndUpdate = async () => {
 	document.querySelector('#msg-info_').classList.remove('hide');
 	document.querySelector('#msg-fail_').classList.add('hide');
@@ -273,16 +293,23 @@ const doSaveAndUpdate = async () => {
         delete el.Rate;
         delete el.Cost;
     });
-    console.log(upsertElements_,upsertElements)
-
+    let deletedElements = lastactivities.get().deletedelements;
+    //console.log(upsertElements_,upsertElements)
    // alert('count upsert = '+upsertElements.length);
    // console.log('upserted',upsertElements);
-    if (upsertElements_.length>0) {
+    if (upsertElements_.length>0 || deletedElements.length>0) {
     try {    
-    const resp = await BPConnection.Activity.upsert(upsertElements_);
+    let itemId = ""; 
+    if (upsertElements_.length>0){
+        const resp = await BPConnection.Activity.upsert(upsertElements_);
+        itemId="" + resp[0].Id + "&";
+    }
+    if (deletedElements.length>0){
+        const resp2 = await BPConnection.Activity.delete(deletedElements);
+    }
     document.querySelector('#msg-succ_').classList.remove('hide');
     window.onbeforeunload = true;
-    window.location = "admin.jsp?name=BILLING_INVOICE_DETAIL_NEW&key=" + resp[0].Id + "&mode=L";
+    window.location = "admin.jsp?name=BILLING_INVOICE_DETAIL_NEW&"+itemId+"mode=L";
     }catch(e){
         console.log(e);
         document.querySelector('#msg-fail_').classList.remove('hide');  
@@ -309,21 +336,63 @@ const checkFieldsFilled = (objActivities,fieldsArr = ['Quantity','Rate']) => {
     return true;
 }
 
-function calculateRate_(row, column, event, scope) {
-    let activityCollection =  lastactivities.get();
+const compareObjects = (o1, o2) => { //one-level comparing (not deep)
+    const comparedProperties = ['ActivityDate','Cost','Id','ProductId','Quantity','Rate','SubscriptionFromDate','SubscriptionToDate','TaxCost','TotalCost'];
+  //  console.log('comparing',o1,o2)
+    for(var p in o1){
+        if(o1.hasOwnProperty(p) && (comparedProperties.includes(p))){
+            if(o1[p] !== o2[p]){
+                console.warn('comp',p);
+                return false;
+            }
+        }
+    }
+    for(var p in o2){
+        if(o2.hasOwnProperty(p) && (comparedProperties.includes(p))){
+            if(o1[p] !== o2[p]){
+                console.warn('comp',p);
+                return false;
+            }
+        }
+    }
+   // console.log('equals');
+    return true;
+};    
+
+const findChangedRecords = () =>{
+    let changedItemsIndex = [];
+    let lastactivities_ = lastactivities.get().elements; 
+    for (let i=0;i<lastactivities_.length;i++){
+        let lastAct = lastactivities_[i];
+        let lastAct_copy = lastactivities_copy[i];
+    //    console.log(lastAct,lastAct_copy)
+        if (!compareObjects(lastAct,lastAct_copy)){
+            changedItemsIndex.push(i)	
+        }
+    }
+    console.log('findChangedRecords>',changedItemsIndex);
+    return changedItemsIndex;
+}
+
+const calculateRate_ = (row,column,event,scope) => {
+    let activityCollection = lastactivities.get();
     const columnsCalc = [2/*,4,6*/];
     let rowElement = activityCollection.elements[row];
-    if (rowElement.Quantity /*&& rowElement.Rate */   && (columnsCalc.includes(column))) {
-
+    const changedRecords = findChangedRecords();
+    const trArr = document.querySelector('.customEmbeddedEditable').querySelectorAll('tbody>tr');    
+    for (let indexRow of changedRecords){
+       trArr[indexRow].classList.add('edited'); 
+    } 
+    if (rowElement.Quantity /*&& rowElement.Rate*/ && (columnsCalc.includes(column))) {
         try {
             var whereClause = "AccountId ="+account.get().Id
                 +" and ProductId =0"+ rowElement.ProductId
                 +" and Quantity ="+ rowElement.Quantity;
             BPConnection.AccountProductQuote.retrieveFilteredAsync(whereClause).single()
                 .done(function (res){
-                    console.log('::::::::::::Rate:::::::::::');
-                    console.log(res);
-                    console.log('::::::::::::Rate END:::::::::::');
+                   // console.log('::::::::::::Rate:::::::::::');
+                   // console.log(res);
+                   // console.log('::::::::::::Rate END:::::::::::');
                     var rateDetails = $.parseXML(res.RateDetails);
                     rowElement.Rate = formatAmount($(rateDetails).find('RateDetailsRow > Rate').text());
                     //rowElement.RatedAmount = formatAmount($(rateDetails).find('RateDetailsRow > RatedAmount').text());
@@ -332,15 +401,15 @@ function calculateRate_(row, column, event, scope) {
                         rowElement.Cost = formatAmount(rowElement.Quantity * rowElement.Rate);
                         rowElement.RateOverride = rowElement.Rate;
                         rowElement.CostOverride = rowElement.Cost;
-                        rowElement.TotalCost = formatAmount(+rowElement.Cost + (Number.isNaN(+rowElement.TaxCost)?0:+rowElement.TaxCost));
-    					return;
+                        rowElement.TotalCost = formatAmount(+rowElement.Cost + (Number.isNaN(+rowElement.TaxCost)?0:+rowElement.TaxCost));  
+        				return;
                     }
                 })
                 .fail(function (fail){console.log(fail.message);
                     //alert(fail.message);
                 });
         } catch (e) {
-            alert('ERROR: '+e);
+          //  alert('ERROR: '+e);
             console.log(e);
         }
     }
@@ -362,15 +431,39 @@ function addActivity(index) {
     const accountId = account.get().Id;
     const invoiceId = invoice.get().Id;
     activitiesData.addNew({}, index + 1); // next item
-    activitiesData.forEach(el=> {
+    lastactivities.get().createdelements.map(el=> {
         el.AccountId = accountId;
         el.InvoiceId = invoiceId;
-        el.ActivityDate = formatDateDB(invoiceDate.start);
-        el.SubscriptionFromDate = formatDateDB(invoiceDate.start);
-        el.SubscriptionToDate = formatDateDB(invoiceDate.end);
+        el.ActivityDate = parseDateTimeToUTC(invoiceDate.start);
+        el.SubscriptionFromDate = parseDateTimeToUTC(invoiceDate.start);
+        el.SubscriptionToDate = parseDateTimeToUTC(invoiceDate.end);
     });  
     window.BPActions.refreshState("activities");
 }
+   function delActivity(index){
+        if (confirm('Do you really want do delete this product?')) {
+            debugger;
+        lastactivities.get().removeFromCollection(index); 
+        lastactivities_copy.splice(index, 1);
+    	window.BPActions.refreshState("activities");
+    }
+ }
+        function checkProductRatig(a,b,entity){
+   //alert(entity.RatingMethodType)
+   //     return false
+    }        
+function checkProductType(index,e){
+    const productTypeColumnIndex = 10;
+    const parentTr = e.target.parentNode;
+    const tdArr = parentTr.childNodes;
+    const typeProduct = tdArr[productTypeColumnIndex].childNodes[0].innerHTML;
+    if (DISABLED_PRODUCTS.includes(String(typeProduct).toUpperCase())){
+    for (let td_ of tdArr){
+     td_.classList.add('disabled');
+    }
+}
+ 
+  }
 BPUI.afterRender = () => {
 //fix top&bottom menus width
 let menus = document.querySelectorAll('.formButtons');
