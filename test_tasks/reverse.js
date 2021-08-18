699aae3c036dfff7f0a8a23d4b10cce87f55b6d5
Ed <edd_undead@tut.by>
2021-08-18 12:48:52 +0300

    Update reverse.js

--- test_tasks/reverse.js
+++ test_tasks/reverse.js
@@ -1,1 +1,6 @@
-
+const invert = str => { 
+    if (str===null || str===undefined) return '';
+    const fStr = String(str);
+    if (fStr.length===1) return fStr;
+    return [...fStr].reverse().join('');
+}
